4339873455bfc2ea9f1859bb95a64b40a54cd490
LTE <lloyd@emelle.me>
2023-04-26 16:18:19 -0400

    discord auth testing

--- src/_data/env.js
+++ src/_data/env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  devMode: process.env.NODE_ENV !== 'production'
+  devMode: process.env.NODE_ENV !== 'production',
+  baseUrl: process.env.BASEURL || 'http%3A%2F%2F127.0.0.1%3A8788'
 }
